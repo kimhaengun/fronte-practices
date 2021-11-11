dd6970da1f3f20efd96098c44b38991d3627e61d
kimhaengun <chxl89@naver.com>
2021-11-11 15:56:52 +0900

    ajax json형식

--- ch08/src/main/java/com/douzone/ch08/controller/api/ApiController.java
+++ ch08/src/main/java/com/douzone/ch08/controller/api/ApiController.java
@@ -1,6 +1,7 @@
 package com.douzone.ch08.controller.api;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -45,6 +46,26 @@ public class ApiController {
 		vo.setNo(1L);
 		vo.setName("나는");
 		vo.setMessage("ajax");
+		
+		return JsonResult.success(vo);
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/post01",method = RequestMethod.POST)
+	public Object post01(GuestBookVo vo) {
+		//service -> repository : db insert 성공한 후...
+		vo.setNo(2L);
+		vo.setPassword("");
+		
+		return JsonResult.success(vo);
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/post02",method = RequestMethod.POST)
+	public Object post02(@RequestBody GuestBookVo vo) {
+		//service -> repository : db insert 성공한 후...
+		vo.setNo(2L);
+		vo.setPassword("");
 		
 		return JsonResult.success(vo);
 	}
