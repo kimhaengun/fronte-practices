f2fe345520216a486375fe6f87736bb65b0826a2
kimhaengun <chxl89@naver.com>
2021-11-15 15:52:09 +0900

    바인딩

--- javascript-practices/es6/ex05.js
+++ javascript-practices/es6/ex05.js
@@ -29,3 +29,33 @@ numbers.forEach(function(n){
 
     process.stdout.write(`${n}:${power(n)}\t`);
 });
+
+//ex02
+console.log("\n----------\n");
+numbers.forEach(n => process.stdout.write(`${n}:${(x=>x*x)(n)}\t`));
+
+//ex03
+console.log("\n----------\n");
+[5,3,15,1045,43,92].forEach(e=>{
+    if(e % 5 == 0){
+        process.stdout.write(`${e}\t`);
+    }
+});
+
+//ex04 - this를 어휘적 바인딩(Lexical Bind)
+console.log("\n----------\n");
+const dooly = {
+    name:'뚤리',
+    friends: ['뚜치','머이콜','또우너','갈동'],
+    
+    printFriends: function(){
+        // this.friends.forEach(function(f){
+        //         console.log(`${this.name}의 친구 ${f}`);
+        // }.bind(this));
+         this.friends.forEach(f =>
+            console.log(`${this.name}의 친구 ${f}`)
+         );
+    }
+}
+
+dooly.printFriends();
